803970515efb505575e790aaa0dbd516072997db
VishwanathChenni <vishwanathchenni@gmail.com>
2024-02-04 21:14:41 +0530

    Code updated

--- Selenium-BDD/src/test/java/StepDefinitions/HomePageSteps.java
+++ Selenium-BDD/src/test/java/StepDefinitions/HomePageSteps.java
@@ -1,6 +1,8 @@
 package StepDefinitions;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.manager.SeleniumManager;
 
 public class HomePageSteps {
@@ -10,4 +12,21 @@ public class HomePageSteps {
         //throw new io.cucumber.java.PendingException();
         System.out.println("Home page is displayed"+ LoginSteps.driver.getTitle());
     }
+    @Given("User is on the home page")
+    public void user_is_on_the_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+    }
+    @When("User enters username and password")
+    public void user_enters_username_and_password(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+//        throw new io.cucumber.java.PendingException();
+    }
+
 }
